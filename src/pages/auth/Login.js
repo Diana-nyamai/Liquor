4fef49d8000a42b36e0fbfd99b21a5beb787b916
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-02 19:04:52 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -5,14 +5,14 @@ export class Login extends Component {
         super(props)
     
         this.state = {
-             date: new Date()
+             name: 'diana'
         }
     }
     
     render() {
         return (
             <div>
-                it is {this.state.date.toLocaleTimeString()}
+               my name is {this.state.name}
             </div>
         )
     }
