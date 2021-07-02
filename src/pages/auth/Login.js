10aaea8099034e9831ef3b1a4e582d027d289463
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-02 20:16:14 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -5,19 +5,24 @@ export class Login extends Component {
         super(props)
     
         this.state = {
-             name: 'diana'
+             isToggleOn: true
         }
+        this.handlechangeName = this.handlechangeName.bind(this);
     }
-    handlechangeName = () =>{
-        this.setState({
-            name: 'ndinda'
-        },console.log('name changed when clicked'))
-    }
+    handleClick(){
+        this.setState(prevstate =>({
+            isToggleOn: !prevstate.isToggleOn
+        })
+
+        )
+    }  
+
+
     render() {
         return (
             <div>
             <h1>click to change the name</h1>
-               <button onClick={this.handlechangeName}>{this.state.name}</button> 
+               <button onClick={this.handleClick}>{this.state.isToggleOn? 'On':'off'}</button> 
             </div>
         )
     }
