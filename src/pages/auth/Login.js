71f93c057b0f2b89b68f3582d2d528ed2a3826c2
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-02 19:00:59 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,14 +1,1 @@
-import React from 'react'
 
-const Login = () => {
-    return (
-        <div>
-            it is {new Date().toLocaleTimeString()}
-        </div>
-    )
-}
-setInterval(Login, 1000)
-
-
-
-export default Login
