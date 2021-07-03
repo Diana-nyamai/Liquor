ef236a8f99b7492af5fd613fd3d8b3938260cdaa
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 17:40:55 +0300

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 
 function Home(props) {
-    
+    const Names = props.Data.map((dat) =>
+    <li>{dat.name}</li>
+    )
+    const loc = props.Data.map((locat) =>
+    <li>{locat.brand}</li>)
     return (
         <div>
-       
+       {Names}
+
+       <div>{loc}</div>
         </div>
+        
     )
 }
 
