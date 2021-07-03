a7aa286e1fc6ca3c07546372a9f6594fa2320a5a
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 14:56:19 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Login() {
-    const names = ['diana','ndinda','nyamai']
+    const names = [1,2,3,6]
+    const doubleName = names.map((name) => <li>{name * 2}</li>)
     return (
         <div>
-            
+            {doubleName}
         </div>
     )
 }
