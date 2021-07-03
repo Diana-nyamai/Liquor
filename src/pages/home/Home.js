6b8da2132a79d52b42e3186ddd7645bfea9dada7
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 15:30:42 +0300

    update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Data from './Data'
 
 function Home() {
     
     return (
         <div>
-            
+            {Data.Data.map((data) => {data})}
         </div>
     )
 }
