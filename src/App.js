1bdc001bac5832a1ffa6d768214756f9dd9045ae
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 14:58:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Login } from './pages/auth';
 
+const numbers = [1,2,3,4]
+
 function App() {
+  
   return (
     <div className="App">
      
-    <Login name="diana"/>
+    <Login />
     </div>
   );
 }
