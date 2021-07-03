d2e9f7c67e381e378d5bb2d7d9742bec2867eb75
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 15:32:59 +0300

    update

--- src/pages/home/data.js
+++ src/pages/home/data.js
@@ -1,19 +1,28 @@
-export default {
-     Data = [
-         {
-      name:'diana',
-      brand:'lv',
-      color:'blue'
-    },
-    {
-        name:'diana',
-        brand:'lv',
-        color:'blue'
-      },
-      {
-        name:'diana',
-        brand:'lv',
-        color:'blue'
-      },u
+import React from 'react'
+
+function Data() {
+    const  Data = [
+        {
+     name:'diana',
+     brand:'lv',
+     color:'blue'
+   },
+   {
+       name:'diana',
+       brand:'lv',
+       color:'blue'
+     },
+     {
+       name:'diana',
+       brand:'lv',
+       color:'blue'
+     },u
 ]
+    return (
+        <div>
+            
+        </div>
+    )
 }
+
+export default Data
