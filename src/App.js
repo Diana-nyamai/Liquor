4737e2da16bcd308fd0aba07271bc18b2cf393f8
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 17:21:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './pages/home/Home';
-
+import Data from './pages/home/Data';
 function App() {
   
+  const Datas = Data
   return (
     <div className="App">
      
-   <Home/>
+   <Home datas={Datas}/>
     </div>
   );
 }
