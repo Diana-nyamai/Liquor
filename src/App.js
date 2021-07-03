13148f2e893b40cbecf02167534d836bdad20852
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 15:40:59 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { Login } from './pages/auth';
+
 
 
 
 
 function App() {
-  const Names = ['diana','ndinda','nyamai']
+  
   return (
     <div className="App">
      
