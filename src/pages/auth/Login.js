f1eabeaf71093487c7f435b4cb292feab8d0f1e4
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-03 15:05:22 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Login() {
-    const names = [1,2,3,6]
+function Login(props) {
+    const names = props.Numbers
     const doubleName = names.map((name) => <li>{name * 2}</li>)
     return (
         <div>
