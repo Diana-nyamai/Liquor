e409d7d56860f286e2df995f81e1b626f35bf36a
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-06 13:04:48 +0300

    update

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,15 +1,1 @@
-import React from 'react'
-
-function Login(props) {
-    const names = props.Names
-    const doubleName = names.map((name) => <li
-    >{name}</li>)
-    return (
-        <div>
-        <ol>{doubleName}</ol>
-            
-        </div>
-    )
-}
-
-export default Login
+rfce
